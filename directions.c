95dcf6a38098881a94e06d778348d0d6989195ad
Inbar Pe'er <inbaronet@gmail.com>
2017-11-02 11:25:05 -0400

    lol

--- directions.c
+++ directions.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+char * list_em_files(struct dirent d){
+  char listed[10000];
+  for(int i=0; i<length; i++){
+    while(dirent);
+    
+    
+    
+int main(){
+  dir *d;
+  d= opendir("hw9_sys");
+  struct dirent *entry;
+  entry = readdir(d);
+  closedir(d);
+  printf("\nStatistics for directory:\n");
+  printf("Total director size: %d Bytes", );
+  printf("Directories:\n %s ", list_em(d));
+    
