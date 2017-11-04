bd99970e7c348c2a0e39225c8d14863f23b18477
Inbar Pe'er <inbaronet@gmail.com>
2017-11-04 15:47:17 -0400

    ugh

--- directions.c
+++ directions.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
-char * list_em_files(struct dirent d){
-  char listed[10000];
-  for(int i=0; i<length; i++){
-    while(dirent);
-    
-    
+#include <dirent.h>
+#include <sys/types.h>
+int get_da_size(char* p){
+  DIR *d = opendir(p);
+  struct dirent* entry;
+  entry = readdir(d);
+  int returnval= 0;
+  while(entry){
+    if(entry-> d_type ==DT_REG){
+      struct stat buffer;
+      stat(entry -> d_name, &buffer);
+      size_t s = buffer.st_size;
+      returnval+=s;
+    }
+    entry = readdir(d);
+  }
+  return returnval;
+}
     
 int main(){
-  dir *d;
-  d= opendir("hw9_sys");
-  struct dirent *entry;
-  entry = readdir(d);
-  closedir(d);
+  DIR *d = opendir(p);
+  struct dirent *f;
+  struct stat buffer;
   printf("\nStatistics for directory:\n");
-  printf("Total director size: %d Bytes", );
-  printf("Directories:\n %s ", list_em(d));
+  printf("Total directory size: %d", get_da_size(".")) ;
+  printf("Directories:\n");
+  d = opendir(".");
+  while(f = readdir(d)){
+    if(f->d_type = DT_DIR){
+      printf("%s\n", f->d_name);
+    }
+  }
+  d=opendir(".");
+  printf("Files:\n");
+  while(f=readdir(d)){
+    if(f->d_type ==DT_REG){
+      stat(f ->d_name, &buffer);
+      printf("%s\n", f->d_name);
+    }
+  }
+  closedir(d);
+  return 0;
+}
+  
+ 
+
+  
+	 
+	 
     
